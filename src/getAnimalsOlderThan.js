e83b983fdbaf2b533d93393423741d08f3f6a0dc
bruno <marqsbruno@gmail.com>
2022-01-27 20:19:33 -0300

    arrumei o req 4 denovo por besteira

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -3,5 +3,5 @@ const data = require('../data/zoo_data');
 function getAnimalsOlderThan(animal, age) {
   const anim = data.species.find((element) => element.name === animal);
   return anim.residents.every((idade) => idade.age >= age);
-};
+}
 module.exports = getAnimalsOlderThan;
