39559af5b305f5201f3c49ca21bea2877846c7bb
bruno <marqsbruno@gmail.com>
2022-01-29 18:33:20 -0300

    req 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,28 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const count = { child: 0, adult: 0, senior: 0 };
+  entrants.forEach((element) => {
+    if (element.age < 18) {
+      count.child += 1;
+    } else if (element.age >= 18 && element.age < 50) {
+      count.adult += 1;
+    } else { count.senior += 1; }
+  });
+  return count;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const { child, adult, senior } = countEntrants(entrants);
+  const values = [];
+  values.push(child * 20.99);
+  values.push(adult * 49.99);
+  values.push(senior * 24.99);
+  const fullValue = values[0] + values[1] + values[2];
+  return fullValue;
 }
 
 module.exports = { calculateEntry, countEntrants };
