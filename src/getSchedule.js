10ea5eee1283b4b46cfdad2176b0dc25022495f3
bruno <marqsbruno@gmail.com>
2022-02-02 12:01:18 -0300

    começo req 8

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,20 +1,30 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
-  const officeHours = {};
+function fullSchedule() {
+  const totalHours = {};
   Object.keys(data.hours).forEach((dia) => {
-    officeHours[dia] = `Open from ${data.hours[dia].open}am until ${data.hours[dia].close}pm`;
+    if (dia === 'Monday') {
+      totalHours[dia] = {
+        officeHour: 'CLOSED',
+        exhibition: 'The zoo will be closed!',
+      };
+    } else {
+      const name1 = species.filter((elem) => elem.availability.includes(dia));
+      const name2 = name1.map((spec) => spec.name);
+      totalHours[dia] = {
+        officeHour: `Open from ${data.hours[dia].open}am until ${data.hours[dia].close}pm`,
+        exhibition: name2,
+      };
+    }
   });
-  const exhibition = [];
-  const a = data.species.filter((spec) => spec.availability.map((dia) => {
-    const weekDay = {dia: dia.includes((Object.keys(data.hours)))};
-    return weekDay;
-  }));
-/*   const a = Object.keys(data.hours).filter((dia) =>
-    data.species.filter((spec) => dia.includes(spec.availability))); */
-
-    console.log('RESULTADO', a);
-  return a;
+  return totalHours;
 }
+
+function getSchedule(scheduleTarget) {
+  if (!scheduleTarget) {
+    return fullSchedule();
+  }
+}
+
 module.exports = getSchedule;
